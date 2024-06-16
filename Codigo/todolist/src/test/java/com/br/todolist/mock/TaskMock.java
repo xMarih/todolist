b2a7fb54158ffe87127ead7639ae5af45cb9fbae
xMarih <marry-29@hotmail.com>
2024-06-16 10:41:53 -0300

    test

--- Codigo/todolist/src/test/java/com/br/todolist/mock/TaskMock.java
+++ Codigo/todolist/src/test/java/com/br/todolist/mock/TaskMock.java
@@ -1,29 +1,16 @@
-package com.br.todolist.mock;
+package com.labdesoft.roteiro01.mock;
 
+import com.labdesoft.roteiro01.entity.Task;
+import com.labdesoft.roteiro01.entity.TaskType;
+import com.labdesoft.roteiro01.entity.Priority;
+import com.labdesoft.roteiro01.entity.TaskStatus;
 
+import java.time.LocalDate;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-
-import com.br.todolist.entity.Task;
-
-import java.util.ArrayList;
-import java.util.List;
 public class TaskMock {
-    public static Page<Task> createTasks() {
-
-        List<Task> taskList = new ArrayList<>();
-        Task task1 = new Task();
-        task1.setId(1L);
-        task1.setDescription("Tarefa 1");
-
-        Task task2 = new Task();
-        task2.setId(2L);
-        task2.setDescription("Tarefa 2");
-
-        taskList.add(task1);
-        taskList.add(task2);
-        Page<Task> pagedResponse = new PageImpl(taskList);
-        return pagedResponse;
+    public static Task createMockTask() {
+        Task task = new Task("Mock Task", TaskType.ENHANCEMENT, LocalDate.now(), 5, Priority.HIGH);
+        task.setStatus(TaskStatus.TODO);
+        return task;
     }
 }
