243a9f8b34eab7edbda5c01d43f2445a27c57e21
xMarih <marry-29@hotmail.com>
2024-06-23 19:18:53 -0300

    @CrossOrigin controller

--- Codigo/todolist/src/main/java/com/labdesoft/roteiro01/controller/TaskController.java
+++ Codigo/todolist/src/main/java/com/labdesoft/roteiro01/controller/TaskController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+
+@CrossOrigin
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
